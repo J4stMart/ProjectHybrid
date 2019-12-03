a92994abf7830cd0dfd184b0efa7009359ecfe07
marnixlicht <mlt350@gmail.com>
2019-12-03 18:33:32 +0100

    WIP Reference

--- project hybrid/Assets/ReferenceScript.cs
+++ project hybrid/Assets/ReferenceScript.cs
@@ -13,6 +13,11 @@ public class ReferenceScript : MonoBehaviour
 
     private Vector3 referenceVector;
 
+    private void Start()
+    {
+        newTargetLocation.position = new Vector3(0f, 0f, 0f);
+    }
+
     // Update is called once per frame
     void Update()
     {
