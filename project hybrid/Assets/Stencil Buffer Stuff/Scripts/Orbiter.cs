5fc892be54d69cc2efe43a9e99779676cdd53f8a
marnixlicht <mlt350@gmail.com>
2019-12-03 14:19:37 +0100

    stencil buffer stuff added

---
+++ project hybrid/Assets/Stencil Buffer Stuff/Scripts/Orbiter.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Orbiter : MonoBehaviour
+{
+    public Transform target;
+    public float speed = 10;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.right * Time.deltaTime * speed);
+        Vector3 direction = target.position - transform.position;
+        transform.rotation = Quaternion.LookRotation(direction);
+    }
+}
---
+++ project hybrid/Assets/Stencil Buffer Stuff/Scripts/Rotator.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotator : MonoBehaviour
+{
+    public float speed = 10f;
+
+
+    void Update()
+    {
+        transform.Rotate(Vector3.forward + Vector3.up + Vector3.right, speed * Time.deltaTime);
+    }
+}
