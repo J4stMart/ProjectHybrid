7296a18d1b42ca61294d5140d632a5d12c3b7c10
marnixlicht <mlt350@gmail.com>
2019-12-04 12:15:15 +0100

    Orbit script changes

--- project hybrid/Assets/Stencil Buffer Stuff/Scripts/Orbiter.cs
+++ project hybrid/Assets/Stencil Buffer Stuff/Scripts/Orbiter.cs
@@ -4,22 +4,9 @@ using System.Collections;
 public class Orbiter : MonoBehaviour
 {
     public Transform target;
-    public Vector3 axis = Vector3.up;
-    public Vector3 desiredPosition;
-    public float radius = 2.0f;
-    public float radiusSpeed = 0.5f;
-    public float rotationSpeed = 80.0f;
-
-    void Start()
-    {
-        transform.position = (transform.position - target.position).normalized * radius + target.position;
-        //radius = 2.0f;
-    }
 
     void Update()
     {
-        transform.RotateAround(target.position, axis, rotationSpeed * Time.deltaTime);
-        desiredPosition = (transform.position - target.position).normalized * radius + target.position;
-        transform.position = Vector3.MoveTowards(transform.position, desiredPosition, Time.deltaTime * radiusSpeed);
+        transform.RotateAround(target.transform.position, target.transform.up, 100 * Time.deltaTime);
     }
 }
