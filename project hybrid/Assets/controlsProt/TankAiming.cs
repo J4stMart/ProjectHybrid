86f16813877f3fb35eb9b105f2b56039b77fa82f
Dutch <carnplaynl@gmail.com>
2019-12-10 15:12:57 +0100

    sabile tank and fancy fixed ark

--- project hybrid/Assets/controlsProt/TankAiming.cs
+++ project hybrid/Assets/controlsProt/TankAiming.cs
@@ -10,32 +10,40 @@ public class TankAiming : MonoBehaviour
     [SerializeField] float upwardArcOffset = 5;
     [SerializeField] Transform arcStartPos;
 
+    public float aaa;
 
     void Update() {
+        
         Vector3 dir = -(trackingPosition.position - transform.position);
         TurretTransform.rotation = Quaternion.LookRotation(dir, transform.up);
         TurretTransform.localRotation = Quaternion.Euler(0,TurretTransform.localRotation.eulerAngles.y,0);
 
-
+        Vector3 arcDir = arcStartPos.position - TurretTransform.position;
+        Vector3 horizontal = new Vector3(arcDir.x, 0, arcDir.z);
+        arc.initialUpWardSpeed = Mathf.Sin(Vector3.Angle(arcDir, horizontal) * Mathf.Deg2Rad) * aaa;
+        arc.initialForwardSpeed = Mathf.Cos(Vector3.Angle(arcDir, horizontal) * Mathf.Deg2Rad) * aaa;
+       
 
         arc.aimDirection = TurretTransform.rotation.eulerAngles.y - 90;
 
         arc.offsetPosition = arcStartPos.position - transform.position;// transform.up * arc.startHeight;
 
+        //arc.offsetRotation = Quaternion.Euler(arcDir);
+
         //arc.initialUpWardSpeed = TurretTransform.rotation.eulerAngles.x + upwardArcOffset;
     }
 
-    Vector2 posOnCircle(int i, int totalPosses, float radius)
-    {
-        float dir = ((2 * Mathf.PI) / totalPosses) * i;
-        return new Vector2(Mathf.Cos(dir) * radius, Mathf.Sin(dir) * radius);
-    }
+    //Vector2 posOnCircle(int i, int totalPosses, float radius)
+    //{
+    //    float dir = ((2 * Mathf.PI) / totalPosses) * i;
+    //    return new Vector2(Mathf.Cos(dir) * radius, Mathf.Sin(dir) * radius);
+    //}
 
-    Vector3 RotatePointAroundPivot(Vector3 point, Vector3 pivot, Quaternion angles)
-    {
-        Vector3 dir = point - pivot; // get point direction relative to pivot
-        dir = angles * dir; // rotate it
-        point = dir + pivot; // calculate rotated point
-        return point; // return it
-    }
+    //Vector3 RotatePointAroundPivot(Vector3 point, Vector3 pivot, Quaternion angles)
+    //{
+    //    Vector3 dir = point - pivot; // get point direction relative to pivot
+    //    dir = angles * dir; // rotate it
+    //    point = dir + pivot; // calculate rotated point
+    //    return point; // return it
+    //}
 }
