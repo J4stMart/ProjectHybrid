6e5dc4e8576db4eaccd9f2efcec1e840cd4f97b3
marnixlicht <mlt350@gmail.com>
2019-12-15 22:34:01 +0100

    Explosion impact particles and sounds added

--- project hybrid/Assets/Scripts/Shell.cs
+++ project hybrid/Assets/Scripts/Shell.cs
@@ -5,6 +5,10 @@ using UnityEngine;
 public class Shell : MonoBehaviour
 {
     Rigidbody rb;
+    public AudioClip explosionSound1;
+    public AudioClip explosionSound2;
+    public AudioClip explosionSound3;
+    public GameObject impactEffect;
 
     private void Start()
     {
@@ -22,12 +26,47 @@ public class Shell : MonoBehaviour
         {
             Debug.Log("Hit");
             collision.transform.GetComponent<HitDetection>().gotHit = true;
-            Destroy(gameObject);
+            StartCoroutine(Explosion());
         }
         //dirty check. change to tag later
         else
         {
-            Destroy(gameObject);
+            StartCoroutine(Explosion());
         }
+    }
+
+    IEnumerator Explosion()
+    {
+        AudioClip explosion;
+        int randomNumber = Random.Range(0, 3);
+        if (randomNumber == 0) {
+            explosion = explosionSound1;
+        }
+        else if (randomNumber == 1)
+        {
+            explosion = explosionSound2;
+        }
+        else
+        {
+            explosion = explosionSound3;
+        }
+        GetComponent<AudioSource>().PlayOneShot(explosion, 1f);
+        GetComponent<CapsuleCollider>().enabled = false;
+        transform.GetChild(0).gameObject.SetActive(false);
+        Instantiate(impactEffect, transform.position + transform.forward *1.1f -transform.up*0.2f, Quaternion.RotateTowards(Quaternion.LookRotation(Vector3.up), transform.rotation, 5));
+
+        yield return new WaitForSeconds(0.01f);
+        Vector3 explosionPos = transform.position + transform.forward*1.1f;
+        Collider[] colliders = Physics.OverlapSphere(explosionPos, 9f);
+        foreach (Collider hit in colliders)
+        {
+            Rigidbody rb = hit.GetComponent<Rigidbody>();
+
+            if (rb != null)
+                rb.AddExplosionForce(800, explosionPos, 9f, 3.0F);
+        }
+
+        yield return new WaitForSeconds(1f);
+        Destroy(gameObject);        
     }
 }
